069a6595b6445c5081a04a2976a9add27bccfb84
Juan Camilo Acosta Arango <ja0335@gmail.com>
2016-03-29 17:23:33 -0500

    Update Macros.h
    CUDA implementation

--- Stream-Vorticity/Macros.h
+++ Stream-Vorticity/Macros.h
@@ -1,24 +1,28 @@
-#pragma once
+#ifndef _MACROS_H_
+#define _MACROS_H_
 
 #define PI 3.14159265358979323846f
 
 // 1/0.1 = 10; 1/0.05 = 20; 1/0.01 = 100; 1/0.005 = 200
-#define N 201 
+#define GRID_SIZE 201 
 #define LID_SPEED 1.0f
 #define REYNOLDS_NUMBER 10000.0f
 #define MAX_SOR_ITERATIONS 100
 #define SOR_TOLERANCE_ERROR 0.001f
 #define DT 0.001f // time step
 
-#define SINGLE_PRECISION 0
 #define CAPTURE_DATA 0
+#define SINGLE_PRECISION 0
 #define USE_CPP_PLOT 0
 #define PLOT_RESOLUTION 1024
+#define USE_CUDA 0
 
-#define IJ(i, j) ((i) + N*(j))
+#define IJ(i, j) ((i) + GRID_SIZE*(j))
 
 #if SINGLE_PRECISION
 #define Real float
 #else
 #define Real double
+#endif
+
 #endif
